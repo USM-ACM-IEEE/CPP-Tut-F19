569016eb24acb136d4c1272259b960af5a363012
BCRevels <31749493+BCRevels@users.noreply.github.com>
2019-09-18 22:27:27 -0500

    Add datatypes lesson
    Also includes minor formatting changes

--- basic_syntx.cpp
+++ basic_syntx.cpp
@@ -5,7 +5,7 @@
 
 // In C++ there are several libraries that allow you
 // to complete various tasks with premade functions.
-#include <iostream> 
+#include <iostream>
 // The iostream library contains several useful functions for doing I/O operations.
 
 using namespace std;
@@ -20,8 +20,9 @@ if multiple versions exist across namespaces. Classes will be covered later.
 "std" is the standard namespace, as the abbreviation implies.
 */
 
-int main() {
-    // This is the main function of the program, it contains everything that will be executed when 
+int main()
+{
+    // This is the main function of the program, it contains everything that will be executed when
     // the compiled executable is ran.
     // There can be only one main() function defined in the program, it will not compile otherwise.
     // Note that not every cpp file needs to have a main() function to compile,
@@ -38,7 +39,7 @@ int main() {
     // The right side does not have to be a variable, it can be a static value or string, a function, or anything
     // else that supports being printed.
 
-    // In C++ double quotes denote a string, think words, (Ex: "a string") 
+    // In C++ double quotes denote a string, think words, (Ex: "a string")
     // and single quotes denotes a single character, think numbers/letters/symbols, (Ex: 'A')
 
     // "endl" is another variable provided by the iostream library, its underlying translation depends on your OS, but
@@ -48,11 +49,11 @@ int main() {
 
     // The main() function is expected to return an int when it completes, denoted by the "int" before "main()"
     // To do this we use the "return" keyword followed by an int.
-    // The most common values to have are 0 or 1. In the Unix standard, 0 represents success and 1-255 
+    // The most common values to have are 0 or 1. In the Unix standard, 0 represents success and 1-255
     // represents an error. Numbers over 255 can cause undefined bahavior and generally 2-255 are reserved
     // for specific errors.
 
-    // Note that in C++ ALL lines of code must end with a semicolon ( ; ), 
+    // Note that in C++ ALL lines of code must end with a semicolon ( ; ),
     // and whitespace is only important for seperating names/keywords/etc.
     // Ex: "int main()" is the same as "int                    main()"
 
---
+++ datatypes.cpp
@@ -0,0 +1,44 @@
+/// C++ Basics Tutorial Semester 1, 2019 - 2020
+
+#include <iostream>
+#include <iomanip> // Allows manipulation of input and output
+// Ignore the following two includes, they are for this demonstration only,
+// you do not yet need to know them.
+#include <cfloat>  // For floating point limits
+#include <climits> // For whole-number limits
+
+using namespace std;
+
+int main()
+{
+    bool bool1; // True/False or 1/0
+    char char1; // ASCII Characters -128 to 127 (x86 and x86-64)
+    unsigned char uchar1; // ASCII Characters 0 to 255 (Power PC and Macintosh)
+    short short1; // 16-bit integers
+    unsigned short ushort1; // 16-bit natural numbers
+    int integer1; // Integers (Size Varies, usually >= 16-bit)
+    unsigned uinteger1; // Natural Numbers (Size Varies, usually >= 16-bit)
+    long long1; // Larger Integers (>= 32-bit)
+    unsigned long ulong1; // Larger Natural Numbers (>= 32-bit)
+    long long int ll1; // Very Large Integers (>= 64-bit)
+    unsigned long long int ull1; // Very Large Natural Numbers (>= 64-bit)
+    float float1; // Single precision real number
+    double double1; // Double precision real number
+    string string1; // An array of chars terminated with the "null" character, essentially holds "human words"
+
+    cout << "I'm a string!" << endl;
+    cout << "\n" << static_cast<int>(CHAR_MIN) << " is the minimum signed char value; " << static_cast<int>(CHAR_MAX) << " is the maximum." << endl;
+    cout << static_cast<int>(UCHAR_MAX) << " is the maximum  unsigned char value; " << endl;
+    cout << "\n" << SHRT_MIN << " is the minimum signed short value; " << SHRT_MAX << " is the maximum." << endl;
+    cout << USHRT_MAX << " is the maximum  unsigned short value; " << endl;
+    cout << "\n" << INT_MIN << " is the minimum signed int value; " << INT_MAX << " is the maximum." << endl;
+    cout << UINT_MAX << " is the maximum  unsigned int value; " << endl;
+    cout << "\n" << LONG_MIN << " is the minimum signed long value; " << LONG_MAX << " is the maximum." << endl;
+    cout << ULONG_MAX << " is the maximum  unsigned long value; " << endl;
+    cout << "\n" << LLONG_MIN << " is the minimum signed long long value; " << LLONG_MAX << " is the maximum." << endl;
+    cout << ULLONG_MAX << " is the maximum  unsigned long long value; " << endl;
+    cout << "\n" << scientific << FLT_MIN << " is the minimum float value; " << fixed << FLT_MAX << " is the maximum." << endl;
+    cout << "\n" << scientific << DBL_MIN << " is the minimum double value; " << fixed << DBL_MAX << " is the maximum." << endl;
+
+    return 0;
+}
